3816b79998eef0b2cedac6ac245407be05e8a8ee
Pamela Lovett <pamela.s.lovett@gmail.com>
2017-05-10 11:29:59 -0600

    add tests for elevator

--- tests/elevator_test.js
+++ tests/elevator_test.js
@@ -1,8 +1,10 @@
+const assert = require('assert');
+
 require('babel-core/register')({
   ignore: /node_modules\/(?!ProjectB)/
 });
 
-const Elevator = require('../elevator').default;
+const {Elevator, Person } = require('../classes').default;
 
 describe('Elevator', function() {
   let elevator = new Elevator();
@@ -15,17 +17,40 @@ describe('Elevator', function() {
     let mockUser = { name: "Brittany", currentFloor: 2, dropOffFloor: 5 };
     elevator.goToFloor(mockUser);
 
-    assert(elevator.currentFloor).equals(5);
-    assert(elevator.motionStatus).equals('idle');
-    assert(elevator.getStops()).equals([2, 5]);
+    assert.equal(elevator.currentFloor, 5);
+    assert.equal(elevator.motionStatus, 'idle');
+    assert.deepEqual(elevator.getStops(), [2, 5]);
   });
 
   it('should bring a rider to a floor below their current floor', () => {
-    let mockUser = { name: "Brittany", currentFloor: 8, dropOffFloor: 3 };
+    let mockUser = { name: "Susan", currentFloor: 8, dropOffFloor: 3 };
     elevator.goToFloor(mockUser);
 
-    assert(elevator.currentFloor).equals(3);
-    assert(elevator.motionStatus).equals('idle');
-    assert(elevator.getStops()).equals([8, 3]);
+    assert.equal(elevator.currentFloor, 3);
+    assert.equal(elevator.motionStatus, 'idle');
+    assert.deepEqual(elevator.getStops(), [8, 3]);
+  });
+
+  //Person A goes up
+  it('should bring a rider to a floor above their current floor', () => {
+    let mockUser = { name: "Pammy", currentFloor: 2, dropOffFloor: 4 };
+    elevator.goToFloor(mockUser);
+
+    console.log(elevator.riders)
+    assert.equal(elevator.currentFloor, 4);
+    assert.equal(elevator.totalFloors, 5)
+    assert.equal(elevator.totalStops, 2)
+    assert.equal(elevator.motionStatus, 'idle');
+    assert.deepEqual(elevator.getStops(), [2, 4]);
+  });
+
+  //Person A goes down
+  it('should bring a rider to a floor below their current floor', () => {
+    let mockUser = { name: "Pammy", currentFloor: 10, dropOffFloor: 2 };
+    elevator.goToFloor(mockUser);
+
+    assert.equal(elevator.currentFloor, 2);
+    assert.equal(elevator.motionStatus, 'idle');
+    assert.deepEqual(elevator.getStops(), [10, 2]);
   });
 });
